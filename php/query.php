ae551cb1598ce24935444959f50e79be0f930d74
Brian Bamsch <bbamsch32@gmail.com>
2013-11-13 23:15:59 -0800

    Update Game List Query
    Send data relevant to listing the game on the page

--- php/query.php
+++ php/query.php
@@ -1,8 +1,8 @@
 <?php
 
-error_reporting(E_ALL); 
+//error_reporting(E_ALL); 
 //error_reporting(E_ALL & ~E_NOTICE | E_STRICT); // Warns on good coding standards
-ini_set("display_errors", "1");
+//ini_set("display_errors", "1");
 
 require_once('db.php');
 
@@ -10,7 +10,23 @@ $mysqli = dbconnect();
 
 if($_POST) {
 	switch($_POST['type']) {
-	case 'game':
+	case 'query_game':
+		if($query = $mysqli->prepare("select game_title, game_description, game_steam_appid from Games")) {
+			$query->execute();
+			$query->bind_result($game_title, $game_description, $game_steam_appid);
+			$i = 0;
+			$games = array();
+			while($query->fetch()) {
+				if($game_steam_appid) {
+					$game_image = "http://cdn.steampowered.com/v/gfx/apps/" . $game_steam_appid . "/header.jpg";
+				}
+				$games[$i++] = array(	"title"=>$game_title,
+						 	"description"=>$game_description,
+							"image"=>$game_image);
+			}
+			$query->close();
+		}
+		echo json_encode($games);
 		break;
 	default:
 		die("Unknown Query Type");
