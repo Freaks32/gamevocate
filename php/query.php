718c89885a15fa5c65a3812bc99d379115b1fa56
Brian Bamsch <bbamsch32@gmail.com>
2013-11-20 15:32:37 -0800

    Update Query to new DB Schema
    Modify query.php to use new column names in
    updated database schema.

--- php/query.php
+++ php/query.php
@@ -1,8 +1,8 @@
 <?php
 
-//error_reporting(E_ALL); 
+error_reporting(E_ALL); 
 //error_reporting(E_ALL & ~E_NOTICE | E_STRICT); // Warns on good coding standards
-//ini_set("display_errors", "1");
+ini_set("display_errors", "1");
 
 require_once('db.php');
 
@@ -11,18 +11,20 @@ $mysqli = dbconnect();
 if($_POST) {
 	switch($_POST['type']) {
 	case 'query_game':
-		if($query = $mysqli->prepare("select game_title, game_description, game_steam_appid from Games")) {
+		if($query = $mysqli->prepare("select g_title, g_description, g_steamappid from Games")) {
 			$query->execute();
-			$query->bind_result($game_title, $game_description, $game_steam_appid);
+			$query->bind_result($g_title, $g_description, $g_steamappid);
 			$i = 0;
 			$games = array();
 			while($query->fetch()) {
-				if($game_steam_appid) {
-					$game_image = "http://cdn.steampowered.com/v/gfx/apps/" . $game_steam_appid . "/header.jpg";
+				if($g_steamappid) {
+					$g_image = "http://cdn.steampowered.com/v/gfx/apps/" . $g_steamappid . "/header.jpg";
+				} else {
+					$g_image = "";
 				}
-				$games[$i++] = array(	"title"=>$game_title,
-						 	"description"=>$game_description,
-							"image"=>$game_image);
+				$games[$i++] = array(	"title"=>$g_title,
+						 	"description"=>$g_description,
+							"image"=>$g_image);
 			}
 			$query->close();
 		}
