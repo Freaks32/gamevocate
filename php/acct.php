11c502655f47b9be02cb41f1231993105b546691
Brian Bamsch <bbamsch32@gmail.com>
2013-12-02 17:48:09 -0800

    Use String for FB UID
    32 Bit Machines Do Not Support ints the size of FB's UID
    Store FB UID as string instead of an int

--- php/acct.php
+++ php/acct.php
@@ -1,8 +1,8 @@
 <?php
 
-//error_reporting(E_ALL); 
+error_reporting(E_ALL); 
 //error_reporting(E_ALL & ~E_NOTICE | E_STRICT); // Warns on good coding standards
-//ini_set("display_errors", "1");
+ini_set("display_errors", "1");
 
 require_once('db.php');
 
@@ -16,13 +16,14 @@ if($_POST) {
 			$classkey = 2;
 			$username = $_POST['username'];
 			$fbuid = $_POST['fbuid'];
-			$query->bind_param("isi", $classkey, $username, $fbuid);			
+			$query->bind_param("iss", $classkey, $username, $fbuid);			
 			$result["success"] = $query->execute();
 			$query->close();
 		} else {
 			$result["success"] = false;
 			$result["message"] = "QUERY FAIL";
 		}
+		$result["type"] = gettype($fbuid);
 		echo json_encode($result);
 		break;
 	default:
