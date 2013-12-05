f933a6211234bce5a691a2e35722bc9891d78874
Brian Bamsch <bbamsch32@gmail.com>
2013-12-04 21:01:15 -0800

    Remove Sensitive Data
    Facebook App Secret Compromised. Will be regenerated.
    Secret to be located in php/fbapi/secret.php
    Added .gitignore to prevent upload of secret.php

--- php/fb.php
+++ php/fb.php
@@ -1,10 +1,12 @@
 <?php
 require_once("fbapi/facebook.php");
+require_once("fbapi/secret.php");
 
 function fbconnect() {
+	global $secret;
 	$config = array(
 		'appId' => '181513272053296',
-		'secret' => '788e017cc0831205296c76b5fd2d24df',
+		'secret' => $secret,
 		'fileUpload' => false, // optional
 		'allowSignedRequest' => false, // optional, but should be set to false for non-canvas apps
 	);
