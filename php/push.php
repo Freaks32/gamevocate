ac0b250790a1eabc10997fb8bd364f2dae720917
Brian Bamsch <bbamsch32@gmail.com>
2013-12-07 17:21:42 -0800

    Minor Tweaks
    Add Studio(s) to Gameinfo Viewport
    Report error when User attempts to Rate Games w/o Logging In

--- php/push.php
+++ php/push.php
@@ -76,113 +76,21 @@ if($_POST) {
 		$review = $_POST['review'];
 		$gamekey = $review['gamekey'];
 		$reviewtype = $review['type'];
+		if($userkey == -1) {
+			$result['success'] = false;
+			$result['message'] = "User Not Logged In";
+		} else {
+			switch($reviewtype) {
+			case 'rating':
+				// Rating Only
+				$rating = $review['rating'];
 
-		switch($reviewtype) {
-		case 'rating':
-			// Rating Only
-			$rating = $review['rating'];
-
-			$subresult = checkReviewExists($userkey, $gamekey);
-			if($subresult['success']) {
-				if($subresult['exists']) {
-					// Update when Review Exists
-					if($query = $mysqli->prepare("update Reviews set r_rating = ? where userkey = ? and gamekey = ?")) {
-						$query->bind_param("iii", $rating, $userkey, $gamekey);
-						if(!$query->execute()) {
-							// If Query fails to execute, Fail Result
-							$result['success'] = false;
-						}
-						$query->close();
-					} else {
-						die("Unable to Query Database - LINE:" . __LINE__);
-					}
-				} else {
-					// Insert when Review DNE
-					if($query = $mysqli->prepare("insert into Reviews (userkey, gamekey, r_rating) values (?, ?, ?)")) {
-						$query->bind_param("iii", $userkey, $gamekey, $rating);
-						if(!$query->execute()) {
-							// If Query fails to execute, Fail Result
-							$result['success'] = false;
-						}
-						$query->close();
-					} else {
-						die("Unable to Query Database - LINE:" . __LINE__);
-					}
-				}
-			} else {
-				$result['success'] = false;
-				$result = addError($result, $subresult);
-			}
-			break;
-		case 'review':
-			// Review Only
-			$title = $review['title'];
-			$body = $review['body'];
-
-			$subresult = checkReviewExists($userkey, $gamekey);
-			if($subresult['success']) {
-				if($subresult['exists']) {
-					// Update when Review Exists
-					if($query = $mysqli->prepare("update Reviews set r_title = ?, r_body = ? where userkey = ? and gamekey = ?")) {
-						$query->bind_param("ssii", $title, $body, $userkey, $gamekey);
-						if(!$query->execute()) {
-							// If Query fails to execute, Fail Result
-							$result['success'] = false;
-						}
-					} else {
-						die("Unable to Query Database - LINE:" . __LINE__);
-					}
-				} else {
-					// Insert when Review DNE
-					if($query = $mysqli->prepare("insert into Reviews (userkey, gamekey, r_title, r_body) values (?, ?, ?, ?)")) {
-						$query->bind_param("iiss", $userkey, $gamekey, $title, $body);					
-						if(!$query->execute()) {
-							// If Query fails to execute, Fail Result
-							$result['success'] = false;
-						}
-					} else {
-						die("Unable to Query Database - LINE:" . __LINE__);
-					}
-				}
-			} else {
-				$result['success'] = false;
-				$result = addError($result, $subresult);
-			}
-			break;
-		case 'full':
-			// Rating & Review
-			$rating = $review['rating'];
-			$title = $review['title'];
-			$body = $review['body'];
-
-			// Allow By Deafult
-			$permission = true;
-			// If Review requests to Update via Specified Userkey...
-			if($review['userkey'] != null) {
-				// If Userkey is not Current User's Userkey...
-				if($userkey != $review['userkey']) {
-					// Deny By Default
-					$permission = false;
-					// Verify Review Edit Admin Priviledges
-					$subresult = checkPermissions($userkey, $PERMISSION_EDIT);
-					if($subresult['success']) {
-						$permission = true;
-						// Override Userkey
-						$userkey = $review['userkey'];
-					} else {
-						$permission = false;
-						$result = addError($result, $subresult);
-					}
-				}
-			}
-
-			if($permission) {
 				$subresult = checkReviewExists($userkey, $gamekey);
 				if($subresult['success']) {
 					if($subresult['exists']) {
 						// Update when Review Exists
-						if($query = $mysqli->prepare("update Reviews set r_rating = ?, r_title = ?, r_body = ? where userkey = ? and gamekey = ?")) {
-							$query->bind_param("issii", $rating, $title, $body, $userkey, $gamekey);
+						if($query = $mysqli->prepare("update Reviews set r_rating = ? where userkey = ? and gamekey = ?")) {
+							$query->bind_param("iii", $rating, $userkey, $gamekey);
 							if(!$query->execute()) {
 								// If Query fails to execute, Fail Result
 								$result['success'] = false;
@@ -192,15 +100,13 @@ if($_POST) {
 							die("Unable to Query Database - LINE:" . __LINE__);
 						}
 					} else {
-						$result["INN"] = "DNE";
 						// Insert when Review DNE
-						if($query = $mysqli->prepare("insert into Reviews (userkey, gamekey, r_rating, r_title, r_body) values (?, ?, ?, ?, ?)")) {
-							$query->bind_param("iiiss", $userkey, $gamekey, $rating, $title, $body);					
+						if($query = $mysqli->prepare("insert into Reviews (userkey, gamekey, r_rating) values (?, ?, ?)")) {
+							$query->bind_param("iii", $userkey, $gamekey, $rating);
 							if(!$query->execute()) {
 								// If Query fails to execute, Fail Result
 								$result['success'] = false;
 							}
-							$result["OUT"] = "CLOSE";
 							$query->close();
 						} else {
 							die("Unable to Query Database - LINE:" . __LINE__);
@@ -210,13 +116,111 @@ if($_POST) {
 					$result['success'] = false;
 					$result = addError($result, $subresult);
 				}
-			} else {
-				$result['success'] = false;
-				$result['message'] = "Insufficient User Permission";
+				break;
+			case 'review':
+				// Review Only
+				$title = $review['title'];
+				$body = $review['body'];
+
+				$subresult = checkReviewExists($userkey, $gamekey);
+				if($subresult['success']) {
+					if($subresult['exists']) {
+						// Update when Review Exists
+						if($query = $mysqli->prepare("update Reviews set r_title = ?, r_body = ? where userkey = ? and gamekey = ?")) {
+							$query->bind_param("ssii", $title, $body, $userkey, $gamekey);
+							if(!$query->execute()) {
+								// If Query fails to execute, Fail Result
+								$result['success'] = false;
+							}
+						} else {
+							die("Unable to Query Database - LINE:" . __LINE__);
+						}
+					} else {
+						// Insert when Review DNE
+						if($query = $mysqli->prepare("insert into Reviews (userkey, gamekey, r_title, r_body) values (?, ?, ?, ?)")) {
+							$query->bind_param("iiss", $userkey, $gamekey, $title, $body);					
+							if(!$query->execute()) {
+								// If Query fails to execute, Fail Result
+								$result['success'] = false;
+							}
+						} else {
+							die("Unable to Query Database - LINE:" . __LINE__);
+						}
+					}
+				} else {
+					$result['success'] = false;
+					$result = addError($result, $subresult);
+				}
+				break;
+			case 'full':
+				// Rating & Review
+				$rating = $review['rating'];
+				$title = $review['title'];
+				$body = $review['body'];
+
+				// Allow By Deafult
+				$permission = true;
+				// If Review requests to Update via Specified Userkey...
+				if($review['userkey'] != null) {
+					// If Userkey is not Current User's Userkey...
+					if($userkey != $review['userkey']) {
+						// Deny By Default
+						$permission = false;
+						// Verify Review Edit Admin Priviledges
+						$subresult = checkPermissions($userkey, $PERMISSION_EDIT);
+						if($subresult['success']) {
+							$permission = true;
+							// Override Userkey
+							$userkey = $review['userkey'];
+						} else {
+							$permission = false;
+							$result = addError($result, $subresult);
+						}
+					}
+				}
+
+				if($permission) {
+					$subresult = checkReviewExists($userkey, $gamekey);
+					if($subresult['success']) {
+						if($subresult['exists']) {
+							// Update when Review Exists
+							if($query = $mysqli->prepare("update Reviews set r_rating = ?, r_title = ?, r_body = ? where userkey = ? and gamekey = ?")) {
+								$query->bind_param("issii", $rating, $title, $body, $userkey, $gamekey);
+								if(!$query->execute()) {
+									// If Query fails to execute, Fail Result
+									$result['success'] = false;
+								}
+								$query->close();
+							} else {
+								die("Unable to Query Database - LINE:" . __LINE__);
+							}
+						} else {
+							$result["INN"] = "DNE";
+							// Insert when Review DNE
+							if($query = $mysqli->prepare("insert into Reviews (userkey, gamekey, r_rating, r_title, r_body) values (?, ?, ?, ?, ?)")) {
+								$query->bind_param("iiiss", $userkey, $gamekey, $rating, $title, $body);					
+								if(!$query->execute()) {
+									// If Query fails to execute, Fail Result
+									$result['success'] = false;
+								}
+								$result["OUT"] = "CLOSE";
+								$query->close();
+							} else {
+								die("Unable to Query Database - LINE:" . __LINE__);
+							}
+						}
+					} else {
+						$result['success'] = false;
+						$result = addError($result, $subresult);
+					}
+				} else {
+					$result['success'] = false;
+					$result['message'] = "Insufficient User Permission";
+				}
+				break;
+			default:
+				die("Unsupported Review Type - LINE:" . __LINE__);
 			}
-			break;
-		default:
-			die("Unsupported Review Type - LINE:" . __LINE__);
 		}
 		break;
 	case "push_delete_review":
